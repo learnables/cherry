1ce06de2eedb3b4014a3e565d3f09fb98c008274
seba-1511 <seba-1511@hotmail.com>
2018-11-30 11:04:15 -0800

    Added the barebones of experience replay.

---
+++ cherry/__init__.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python3
+
+from .experience_replay import ExperienceReplay
---
+++ cherry/experience_replay.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+
+import torch as th
+
+from cherry.utils import totensor
+
+
+class ExperienceReplay(object):
+
+    def __init__(self,
+                 states=None,
+                 actions=None,
+                 rewards=None,
+                 next_states=None,
+                 dones=None):
+        self.storage = {
+            'states': [] if states is None else states,
+            'actions': [] if actions is None else actions,
+            'rewards': [] if rewards is None else rewards,
+            'next_states': [] if next_states is None else next_states,
+            'dones': [] if dones is None else dones,
+        }
+
+    def _access_property(self, name):
+        values = self.storage[name]
+        true_size = values[0].size()[1:]
+        return th.cat(values, dim=0).view(len(values), *true_size)
+
+    @property
+    def states(self):
+        return self._access_property('states')
+
+    @property
+    def actions(self):
+        return self._access_property('actions')
+
+    @property
+    def rewards(self):
+        return self._access_property('rewards')
+
+    @property
+    def next_states(self):
+        return self._access_property('next_states')
+
+    @property
+    def dones(self):
+        return self._access_property('dones')
+
+    @property
+    def list_states(self):
+        return self.storage['states']
+
+    @property
+    def list_actions(self):
+        return self.storage['actions']
+
+    @property
+    def list_rewards(self):
+        return self.storage['rewards']
+
+    @property
+    def list_next_states(self):
+        return self.storage['next_states']
+
+    @property
+    def list_dones(self):
+        return self.storage['dones']
+
+    def add(self, state, action, reward, next_state, done):
+        self.storage['states'].append(totensor(state))
+        self.storage['actions'].append(totensor(action))
+        self.storage['rewards'].append(totensor(reward))
+        self.storage['next_states'].append(totensor(next_state))
+        self.storage['dones'].append(totensor(done))
+
+    def empty(self):
+        self = self.__init__()
