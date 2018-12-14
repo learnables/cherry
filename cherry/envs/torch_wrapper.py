1076383789727a0120dda0c2cfc1e1ed403951a1
seba-1511 <seba-1511@hotmail.com>
2018-12-13 22:50:59 -0800

    Added GAE, fixed some bugs

--- cherry/envs/torch_wrapper.py
+++ cherry/envs/torch_wrapper.py
@@ -25,6 +25,7 @@ class Torch(Wrapper):
     def _convert_state(self, state):
         if isinstance(state, (float, int)):
             state = np.array([state])
+        state = np.array([state])
         return th.from_numpy(state).float().unsqueeze(0)
 
     def step(self, action):
--- cherry/rewards.py
+++ cherry/rewards.py
@@ -1,15 +1,12 @@
 #!/usr/bin/env python3
 
 
-def discount_rewards(gamma, rewards, dones):
+def discount_rewards(gamma, rewards, dones, bootstrap=0.0):
     """
-    TODO: Assuming we're given a partial trajectory,
-    I think that the first R should be bootstraped from the value function.
-    Otherwise, you might have an all 0 discounted rewards.
-    (IIRC, that's how they do it in A2C)
-    But is it here the best place to take care of this ?
+    rewards: 0.1119, 0.1131, 0.1142, 0.1154, 0.1165
+    advantages: 0.1120, 0.1132, 0.1143, 0.1155, 0.1166
     """
-    R = 0.0
+    R = bootstrap
     discounted = []
     for reward, done in zip(rewards[::-1], dones[::-1]):
         if done:
@@ -17,3 +14,30 @@ def discount_rewards(gamma, rewards, dones):
         R = reward + gamma * R
         discounted.insert(0, R)
     return discounted
+
+
+def generalized_advantage_estimate(gamma, tau, rewards, dones, values, bootstrap=0.0):
+    """
+    rewards: 0.1119, 0.1131, 0.1142, 0.1154, 0.1165
+    advantages: -0.1806, -0.1602, -0.1278, -0.1558, -0.0566
+    """
+    msg = 'GAE requires the value of the next state to be appended.'
+    assert len(values) > len(rewards), msg
+    R = bootstrap
+    advantage = 0.0
+    discounted = []
+    advantages = []
+    for i in reversed(range(len(rewards))):
+        if dones[i]:
+            R = 0.0
+            advantage = 0.0
+        R = rewards[i] + gamma * R
+        discounted.insert(0, R)
+        td_error = rewards[i] + gamma * values[i+1] - values[i]
+        advantage = advantage * tau * gamma + td_error
+        advantages.insert(0, advantage)
+
+    return discounted, advantages
+
+discount = discount_rewards
+gae = generalized_advantage_estimate
