Fixes environments ordering and normalization.

cherry/envs/openai_atari_wrapper.py
@@ -14,15 +14,9 @@ Inspired from OpenAI's baselines:
 
 https://github.com/openai/baselines/blob/master/baselines/common/atari_wrappers.py
 
-and from Shangtong Zhang's DeepRL:
+and from Ilya Kostrikov's implementation:
 
-https://github.com/ShangtongZhang/DeepR/
-
-#######################################################################
-# Copyright (C) 2017 Shangtong Zhang(zhangshangtong.cpp@gmail.com)    #
-# Permission given to modify the code as long as you keep this        #
-# declaration at the top                                              #
-#######################################################################
+https://github.com/ikostrikov/pytorch_a2c_ppo_acktr/
 
 The MIT License
 
@@ -169,7 +163,7 @@ class MaxAndSkipEnv(Wrapper):
         return self.env.reset(**kwargs)
 
 
-class ClipRewardEnv(gym.RewardWrapper):
+class ClipRewardEnv(Wrapper, gym.RewardWrapper):
     def __init__(self, env):
         gym.RewardWrapper.__init__(self, env)
 
@@ -207,7 +201,7 @@ class FrameStack_(Wrapper):
         self.k = k
         self.frames = deque([], maxlen=k)
         shp = env.observation_space.shape
-        self.observation_space = spaces.Box(low=0, high=255, shape=(shp[0], shp[1], shp[2] * k), dtype=np.uint8)
+        self.observation_space = spaces.Box(low=0, high=255, shape=(shp[0] * k, shp[1], shp[2]), dtype=np.uint8)
 
     def reset(self):
         ob = self.env.reset()
@@ -316,10 +310,10 @@ class OpenAIAtari(Wrapper):
         env = MaxAndSkipEnv(env, skip=4)
         env = wrap_deepmind(env,
                             episode_life=True,
-                            clip_rewards=False,
+                            clip_rewards=True,
                             frame_stack=False,
                             scale=False,
-        )
+                            )
         obs_shape = env.observation_space.shape
         if len(obs_shape) == 3:
             env = TransposeImage(env)

cherry/envs/openai_normalize_wrapper.py
@@ -96,7 +96,7 @@ class OpenAINormalize(Wrapper):
     def step(self, action):
         state, reward, done, info = self.env.step(action)
         self.ret = self.gamma * self.ret + reward
-        state = self._obfilt(state.reshape(1, -1))[0]
+        state = self._obfilt(state)
         if self.ret_rms:
             reward = np.array([[reward]])
             self.ret_rms.update(self.ret)

examples/distributed_atari/models.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import torch as th
 import torch.nn as nn
 from torch.distributions import Categorical
 
@@ -37,12 +38,10 @@ class NatureCNN(nn.Module):
             nn.ReLU(),
             init_(nn.Conv2d(32, 64, 4, stride=2)),
             nn.ReLU(),
-#            init_(nn.Conv2d(64, 32, 3, stride=1)),
-            init_(nn.Conv2d(64, 64, 3, stride=1)),
+            init_(nn.Conv2d(64, 32, 3, stride=1)),
             nn.ReLU(),
             Flatten(),
-#            init_(nn.Linear(32 * 7 * 7, hidden_size)),
-            init_(nn.Linear(64 * 7 * 7, hidden_size)),
+            init_(nn.Linear(32 * 7 * 7, hidden_size)),
             nn.ReLU()
         )
 
@@ -61,6 +60,20 @@ class NatureCNN(nn.Module):
         self.actor_linear.weight.data.mul_(1e-3)
 
         self.train()
+
+        weights = th.load('./model.pth')
+        self.features[0].weight.data.copy_(weights['base.main.0.weight'])
+        self.features[0].bias.data.copy_(weights['base.main.0.bias'])
+        self.features[2].weight.data.copy_(weights['base.main.2.weight'])
+        self.features[2].bias.data.copy_(weights['base.main.2.bias'])
+        self.features[4].weight.data.copy_(weights['base.main.4.weight'])
+        self.features[4].bias.data.copy_(weights['base.main.4.bias'])
+        self.features[7].weight.data.copy_(weights['base.main.7.weight'])
+        self.features[7].bias.data.copy_(weights['base.main.7.bias'])
+        self.critic_linear.weight.data.copy_(weights['base.critic_linear.weight'])
+        self.critic_linear.bias.data.copy_(weights['base.critic_linear.bias'])
+        self.actor_linear.weight.data.copy_(weights['dist.linear.weight'])
+        self.actor_linear.bias.data.copy_(weights['dist.linear.bias'])
 
     def forward(self, inputs):
         # Inputs should be 1, 4, 84, 84 for a single state