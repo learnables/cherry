b78a746957c89e26bea701a131d4fc5d4986e43e
seba-1511 <seba-1511@hotmail.com>
2019-03-27 15:49:04 -0700

    Updated docs.

--- cherry/nn/control_layers.py
+++ cherry/nn/control_layers.py
@@ -6,6 +6,23 @@ import cherry as ch
 
 class ControlLinear(nn.Linear):
 
+    """
+    [[Source]]()
+
+    **Description**
+
+    Linear layer for control environments.
+
+    **References**
+
+    **Arguments**
+
+    **Returns**
+
+    **Example**
+
+    """
+
     def __init__(self, *args, **kwargs):
         gain = kwargs.pop('gain', None)
         super(ControlLinear, self).__init__(*args, **kwargs)
