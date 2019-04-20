0b2f9ca4b6959b5ab3c4d6c922d77b5e597b3c9f
seba-1511 <seba-1511@hotmail.com>
2019-04-20 15:24:25 -0700

    Fix CartPole version in benchmarks.

--- benchmarks/run_pybullet.sh
+++ benchmarks/run_pybullet.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-MKL_NUM_THREADS=1 OMP_NUM_THREADS=1 python benchmark.py ../examples/pybullet/ppo_pybullet.py CartPoleBulletEnv-v0 42
+MKL_NUM_THREADS=1 OMP_NUM_THREADS=1 python benchmark.py ../examples/pybullet/ppo_pybullet.py CartPoleBulletEnv-v1 42
 MKL_NUM_THREADS=1 OMP_NUM_THREADS=1 python benchmark.py ../examples/pybullet/ppo_pybullet.py RoboschoolAnt-v1 42
 MKL_NUM_THREADS=1 OMP_NUM_THREADS=1 python benchmark.py ../examples/pybullet/ppo_pybullet.py MinitaurTrottingEnv-v0 42
 MKL_NUM_THREADS=1 OMP_NUM_THREADS=1 python benchmark.py ../examples/pybullet/ppo_pybullet.py AntBulletEnv-v0 42
