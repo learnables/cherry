a3f71f4844f5e3072b2c67eb5c7fb898d4213244
seba-1511 <seba-1511@hotmail.com>
2019-04-24 15:35:47 -0700

    Minors.

--- benchmarks/run_atari.sh
+++ benchmarks/run_atari.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
-MKL_NUM_THREADS=1 OMP_NUM_THREADS=1 BENCH_SCRIPT=../examples/atari/a2c_atari.py BENCH_ENV=PongNoFrameskip-v4     BENCH_SEED=42 python benchmark.py &
-MKL_NUM_THREADS=1 OMP_NUM_THREADS=1 BENCH_SCRIPT=../examples/atari/a2c_atari.py BENCH_ENV=BreakoutNoFrameskip-v4 BENCH_SEED=42 python benchmark.py &
-MKL_NUM_THREADS=1 OMP_NUM_THREADS=1 BENCH_SCRIPT=../examples/atari/a2c_atari.py BENCH_ENV=EnduroNoFrameskip-v4   BENCH_SEED=42 python benchmark.py &
-MKL_NUM_THREADS=1 OMP_NUM_THREADS=1 BENCH_SCRIPT=../examples/atari/a2c_atari.py BENCH_ENV=SeaquestNoFrameskip-v4 BENCH_SEED=42 python benchmark.py &
-wait
+CUDA_VISIBLE_DEVICES=2 MKL_NUM_THREADS=1 OMP_NUM_THREADS=1 BENCH_SCRIPT=../examples/atari/ppo_atari.py BENCH_ENV=PongNoFrameskip-v4 BENCH_SEED=42 python benchmark.py
+
+#MKL_NUM_THREADS=1 OMP_NUM_THREADS=1 BENCH_SCRIPT=../examples/atari/a2c_atari.py BENCH_ENV=PongNoFrameskip-v4     BENCH_SEED=42 python benchmark.py &
+#MKL_NUM_THREADS=1 OMP_NUM_THREADS=1 BENCH_SCRIPT=../examples/atari/a2c_atari.py BENCH_ENV=BreakoutNoFrameskip-v4 BENCH_SEED=42 python benchmark.py &
+#MKL_NUM_THREADS=1 OMP_NUM_THREADS=1 BENCH_SCRIPT=../examples/atari/a2c_atari.py BENCH_ENV=EnduroNoFrameskip-v4   BENCH_SEED=42 python benchmark.py &
+#MKL_NUM_THREADS=1 OMP_NUM_THREADS=1 BENCH_SCRIPT=../examples/atari/a2c_atari.py BENCH_ENV=SeaquestNoFrameskip-v4 BENCH_SEED=42 python benchmark.py &
+#wait
+
 
 #MKL_NUM_THREADS=1 \
 #OMP_NUM_THREADS=1 \
