cc251fad153ab1cdee4eb1a5702d2b158fa0a0d6
seba-1511 <seba-1511@hotmail.com>
2019-04-24 17:29:21 -0700

    Add spinup DQN to benchmarks.

--- benchmarks/run_spinup.sh
+++ benchmarks/run_spinup.sh
@@ -4,4 +4,5 @@ MKL_NUM_THREADS=1 OMP_NUM_THREADS=1 BENCH_SCRIPT=../examples/spinning-up/cherry_
 MKL_NUM_THREADS=1 OMP_NUM_THREADS=1 BENCH_SCRIPT=../examples/spinning-up/cherry_ddpg.py BENCH_ENV=Pendulum-v0 BENCH_SEED=42 python benchmark.py &
 MKL_NUM_THREADS=1 OMP_NUM_THREADS=1 BENCH_SCRIPT=../examples/spinning-up/cherry_ppo.py  BENCH_ENV=Pendulum-v0 BENCH_SEED=42 python benchmark.py &
 MKL_NUM_THREADS=1 OMP_NUM_THREADS=1 BENCH_SCRIPT=../examples/spinning-up/cherry_sac.py  BENCH_ENV=Pendulum-v0 BENCH_SEED=42 python benchmark.py &
+MKL_NUM_THREADS=1 OMP_NUM_THREADS=1 BENCH_SCRIPT=../examples/spinning-up/cherry_dqn.py  BENCH_ENV=Pendulum-v0 BENCH_SEED=42 python benchmark.py &
 wait
