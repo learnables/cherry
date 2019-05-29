08cc50de70cd1b1ce8874fe97561d30b02ffd1e0
Sébastien Arnold <seba-1511@hotmail.com>
2019-05-28 21:40:40 -0700

    Add documentation for all modules except envs.

--- cherry/algorithms/sac.py
+++ cherry/algorithms/sac.py
@@ -4,52 +4,181 @@ import torch as th
 from torch.nn import functional as F
 
 
-def policy_loss(log_probs, q_actions, alpha=1.0):
+def policy_loss(log_probs, q_curr, alpha=1.0):
     """
-    Arguments:
+    [[Source]](https://github.com/seba-1511/cherry/blob/master/cherry/algorithms/sac.py)
 
-    * log_probs: the log density of the actions from the current policy on
-               some states.
-    * q_actions: the Q-values for those same actions.
-    * alpha: the weight of the policy entropy.
+    **Description**
+
+    The policy loss of the Soft Actor-Critic.
+    
+    New actions are sampled from the target policy, and those are used to compute the Q-values.
+    While we should back-propagate through the Q-values to the policy parameters, we shouldn't
+    use that gradient to optimize the Q parameters.
+    This is often avoided by either using a target Q function, or by zero-ing out the gradients
+    of the Q function parameters.
+
+    **References**
+    
+    1. SAC papers (1, 2, & 3)
+
+    **Arguments**
+    
+    * **log_probs** (tensor) - Log-density of the selected actions.
+    * **q_curr** (tensor) - Q-values of state-action pairs.
+    * **alpha** (float, *optional*, default=1.0) - Entropy weight.
+
+    **Returns**
+    
+    * (tensor) - The policy loss for the given arguments.
+
+    **Example**
+
+    ~~~python
+    densities = policy(batch.state())
+    actions = densities.sample()
+    log_probs = densities.log_prob(actions)
+    q_curr = q_function(batch.state(), actions)
+    loss = policy_loss(log_probs, q_curr, alpha=0.1)
+    ~~~
+    
     """
-    msg = 'log_probs and q_actions must have equal size.'
-    assert log_probs.size() == q_actions.size(), msg
-    return th.mean(alpha * log_probs - q_actions)
+    msg = 'log_probs and q_curr must have equal size.'
+    assert log_probs.size() == q_curr.size(), msg
+    return th.mean(alpha * log_probs - q_curr)
 
 
-def action_value_loss(q_old_pred, v_next, rewards, dones, gamma):
+def action_value_loss(q_old, v_next, rewards, dones, gamma):
     """
-    Arguments:
+    [[Source]](https://github.com/seba-1511/cherry/blob/master/cherry/algorithms/sac.py)
 
-    * q_old_pred: Q values on an existing transition.
-    * v_next: V values for the resulting state.
-    * rewards: observed rewards during the transition.
-    * dones: which states were terminal.
-    * gamma: discount factor.
+    **Description**
+
+    The action-value loss of the Soft Actor-Critic.
+    
+    For a given transition, the Q-values and V-values are recomputed on the actual transition data.
+    The we should not back-propagate through the V-values, and often use a target network for them.
+
+    **References**
+    
+    1. SAC papers (1, 2, & 3)
+
+    **Arguments**
+    
+    * **q_old** (tensor) - Action values of the actual transition.
+    * **v_next** (tensor) - State values of the resulting state.
+    * **rewards** (tensor) - Observed rewards of the transition.
+    * **dones** (tensor) - Which states were terminal.
+    * **gamma** (float) - Discount factor.
+
+    **Returns**
+    
+    * (tensor) - The policy loss for the given arguments.
+
+    **Example**
+
+    ~~~python
+    q_old = qf(batch.state(), batch.action().detach())
+    v_next = targe_vf(batch.next_state())
+    loss = action_value_loss(q_old,
+                             v_next,
+                             batch.reward(),
+                             batch.done(),
+                             gamma=0.99)
+    ~~~
+    
     """
     msg = 'v_next, rewards, and dones must have equal size.'
     assert rewards.size() == dones.size() == v_next.size(), msg
     q_target = rewards + (1.0 - dones) * gamma * v_next
-    return F.mse_loss(q_old_pred, q_target.detach())
+    return F.mse_loss(q_old, q_target.detach())
 
 
-def state_value_loss(v_pred, log_probs, q_values, alpha=1.0):
+def state_value_loss(v_curr, log_probs, q_curr, alpha=1.0):
     """
-    Arguments:
+    [[Source]](https://github.com/seba-1511/cherry/blob/master/cherry/algorithms/sac.py)
 
-    * v_pred: the V values of states from a batch.
-    * log_probs: the log density of actions from the current policy on
-               those states.
-    * q_values: the Q values of the those actions on those states.
-    * alpha: the weight of the policy entropy.
+    **Description**
+
+    The state-value loss of the Soft Actor-Critic.
+    
+    This update is computed "on-policy": states are sampled from a replay but the state values,
+    action values, and log-densities are computed using the current value functions and policy.
+
+    **References**
+    
+    1. SAC papers (1, 2, & 3)
+
+    **Arguments**
+    
+    * **v_curr** (tensor) - State values for some observed states.
+    * **log_probs** (tensor) - Log-density of actions sampled from the current policy.
+    * **q_curr** (tensor) - Action values of the actions for the current policy.
+    * **alpha** (float, *optional*, default=1.0) - Entropy weight.
+
+    **Returns**
+    
+    * (tensor) - The state value loss for the given arguments.
+
+    **Example**
+
+    ~~~python
+    densities = policy(batch.state())
+    actions = densities.sample()
+    log_probs = densities.log_prob(actions)
+    q_curr = qf(batch.state(), actions)
+    v_curr = vf(batch.state())
+    loss = state_value_loss(v_curr,
+                            log_probs,
+                            q_curr,
+                            alpha=0.1)
+    ~~~
+    
     """
-    msg = 'v_pred, q_values, and log_probs must have equal size.'
-    assert v_pred.size() == q_values.size() == log_probs.size(), msg
-    v_target = q_values - alpha * log_probs
-    return F.mse_loss(v_pred, v_target.detach())
+    msg = 'v_curr, q_curr, and log_probs must have equal size.'
+    assert v_curr.size() == q_curr.size() == log_probs.size(), msg
+    v_target = q_curr - alpha * log_probs
+    return F.mse_loss(v_curr, v_target.detach())
 
 
 def entropy_weight_loss(log_alpha, log_probs, target_entropy):
+    """
+    [[Source]](https://github.com/seba-1511/cherry/blob/master/cherry/algorithms/sac.py)
+
+    **Description**
+
+    Loss of the entropy weight, to automatically tune it.
+    
+    The target entropy needs to be manually tuned.
+    However, a popular heuristic for TanhNormal policies is to use the negative of the action-space
+    dimensionality. (e.g. -4 when operating the voltage of a quad-rotor.)  
+
+    **References**
+    
+    1. SAC paper 3
+
+    **Arguments**
+    
+    * **log_alpha** (tensor) - Log of the entropy weight.
+    * **log_probs** (tensor) - Log-density of policy actions.
+    * **target_entropy** (float) - Target of the entropy value.
+
+    **Returns**
+    
+    * (tensor) - The state value loss for the given arguments.
+
+    **Example**
+
+    ~~~python
+    densities = policy(batch.state())
+    actions = densities.sample()
+    log_probs = densities.log_prob(actions)
+    target_entropy = -np.prod(env.action_space.shape).item()
+    loss = entropy_weight_loss(alpha.log(),
+                               log_probs,
+                               target_entropy)
+    ~~~
+    
+    """
     loss = -(log_alpha * (log_probs + target_entropy).detach())
     return loss.mean()
--- cherry/nn/init.py
+++ cherry/nn/init.py
@@ -7,19 +7,31 @@ import torch.nn as nn
 
 def pong_control_(module, bias=0.1):
     """
-    [[Source]]()
+    [[Source]](https://github.com/seba-1511/cherry/blob/master/cherry/nn/init.py)
 
     **Description**
 
-    Control initialization from Vitchyr Pong's implementations.
+    The default initialization for robotic control of RLkit.
 
-    **References**
+    **Credit**
+    
+    Adapted from Vitchyr Pong's implementations.
 
     **Arguments**
+    
+    * **module** (nn.Module) - Module to initialize.
+    * **bias** (float, *optional*, default=0.1) - Constant bias initialization.
 
     **Returns**
+    
+    * Module, whose weight and bias have been modified in-place.
 
     **Example**
+    
+    ~~~python
+    linear = nn.Linear(23, 5)
+    pong_control_(linear)
+    ~~~
 
     """
     weight = module.weight
@@ -38,19 +50,31 @@ def pong_control_(module, bias=0.1):
 
 def kostrikov_control_(module, gain=None):
     """
-    [[Source]]()
+    [[Source]](https://github.com/seba-1511/cherry/blob/master/cherry/nn/init.py)
 
     **Description**
 
-    Control initialization from Ilya Kostrikov's implementations.
+    Default initialization for robotic control.
 
-    **References**
+    **Credit**
+    
+    Adapted from Ilya Kostrikov's implementation, itself inspired from OpenAI Baslines.
 
     **Arguments**
+    
+    * **module** (nn.Module) - Module to initialize.
+    * **gain** (float, *optional*, default=sqrt(2.0)) - Gain of orthogonal initialization.
 
     **Returns**
+    
+    * Module, whose weight and bias have been modified in-place.
 
     **Example**
+    
+    ~~~python
+    linear = nn.Linear(23, 5)
+    kostrikov_control_(linear)
+    ~~~
 
     """
     with th.no_grad():
@@ -63,19 +87,32 @@ def kostrikov_control_(module, gain=None):
 
 def atari_init_(module, gain=None):
     """
-    [[Source]]()
-
+    [[Source]](https://github.com/seba-1511/cherry/blob/master/cherry/nn/init.py)
+    
     **Description**
 
-    Atari initialization from Ilya Kostrikov's implementations.
+    Default initialization for Atari environments.
 
-    **References**
+    **Credit**
+    
+    Adapted from Ilya Kostrikov's implementation, itself inspired from OpenAI Baslines.
 
     **Arguments**
+    
+    * **module** (nn.Module) - Module to initialize.
+    * **gain** (float, *optional*, default=None) - Gain of orthogonal initialization.
+    Default is computed for ReLU activation with `torch.nn.init.calculate_gain('relu')`.
 
     **Returns**
+    
+    * Module, whose weight and bias have been modified in-place.
 
     **Example**
+    
+    ~~~python
+    linear = nn.Linear(23, 5)
+    atari_init_(linear)
+    ~~~
 
     """
     if gain is None:
