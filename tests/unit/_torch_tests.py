ec88bac03bf3ac3fae1010c5db8329db595dc5d6
Niklas Goby <ngoby@geospin.de>
2020-02-14 11:03:54 +0100

    Fix typo and added some basic tests. (issue #22)

--- tests/unit/_torch_tests.py
+++ tests/unit/_torch_tests.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import unittest
+import numpy as np
 import torch as th
 import cherry as ch
 
@@ -17,12 +18,36 @@ class TestTorch(unittest.TestCase):
         ref = th.zeros(1, DIM)
         ref[0, single] += 1
         self.assertTrue((oh_single - ref).pow(2).sum().item() == 0)
-        
+
+        single = np.int64(3)
+        oh_single = ch.onehot(single, dim=DIM)
+        self.assertTrue(oh_single.size(0) == 1)
+        self.assertTrue(oh_single.size(1) == DIM)
+        ref = th.zeros(1, DIM)
+        ref[0, single] += 1
+        self.assertTrue((oh_single - ref).pow(2).sum().item() == 0)
+
+        single = np.float64(3.0)
+        oh_single = ch.onehot(single, dim=DIM)
+        self.assertTrue(oh_single.size(0) == 1)
+        self.assertTrue(oh_single.size(1) == DIM)
+        ref = th.zeros(1, DIM)
+        ref[0, int(single)] += 1
+        self.assertTrue((oh_single - ref).pow(2).sum().item() == 0)
+
+        single = np.float64(3.2)
+        oh_single = ch.onehot(single, dim=DIM)
+        self.assertTrue(oh_single.size(0) == 1)
+        self.assertTrue(oh_single.size(1) == DIM)
+        ref = th.zeros(1, DIM)
+        ref[0, int(single)] += 1
+        self.assertTrue((oh_single - ref).pow(2).sum().item() == 0)
+
         multi = th.arange(DIM)
         multi = ch.onehot(multi, dim=DIM)
         ref = th.eye(DIM)
         self.assertTrue((multi - ref).pow(2).sum().item() == 0)
-        
-        
+
+
 if __name__ == '__main__':
     unittest.main()
