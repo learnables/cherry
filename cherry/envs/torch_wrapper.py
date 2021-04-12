67ca2118bad1a3e0865b625d6e25372f46ae7736
seba-1511 <seba-1511@hotmail.com>
2021-04-12 13:38:26 -0700

    Add device and device_env to Torch wrapper.

--- cherry/envs/torch_wrapper.py
+++ cherry/envs/torch_wrapper.py
@@ -22,6 +22,11 @@ class Torch(Wrapper):
         env.step(action)
     """
 
+    def __init__(self, env, device='cpu', env_device='cpu'):
+        super(Torch, self).__init__(env)
+        self.device = device
+        self.env_device = env_device
+
     def _convert_state(self, state):
         if isinstance(state, (float, int)):
             state = ch.totensor(state)
@@ -37,6 +42,7 @@ class Torch(Wrapper):
 
     def _convert_atomic_action(self, action):
         if isinstance(action, th.Tensor):
+            action = action.to(self.env_device)
             action = action.view(-1).cpu().detach().numpy()
         if self.discrete_action:
             if not isinstance(action, (int, float)):
@@ -59,11 +65,13 @@ class Torch(Wrapper):
         action = self._convert_action(action)
         state, reward, done, info = self.env.step(action)
         state = self._convert_state(state)
+        state = state.to(self.device)
         return state, reward, done, info
 
     def reset(self, *args, **kwargs):
         state = self.env.reset(*args, **kwargs)
         state = self._convert_state(state)
+        state = state.to(self.device)
         return state
 
     def seed(self, *args, **kwargs):
